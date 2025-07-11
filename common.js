6e32ce6bfd4d78390152fa5f15bb917e1719639a
yogesh3656 <97842120+yogesh3656@users.noreply.github.com>
2025-07-11 16:28:05 +0530

    Update common.js

--- common.js
+++ common.js
@@ -8,3 +8,24 @@ window.addEventListener("load", function(){
     }
   });
 });
+
+//------------------------"table finished"/"floating btn start"----------------------------------
+// Get the button
+let mybutton = document.getElementById(&quot;myBtn&quot;);
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop &gt; 20 || document.documentElement.scrollTop &gt; 20) {
+    mybutton.style.display = &quot;block&quot;;
+  } else {
+    mybutton.style.display = &quot;none&quot;;
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
