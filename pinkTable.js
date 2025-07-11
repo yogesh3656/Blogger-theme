9ee0e121efdb7180858fea86bae69472fade5184
yogesh3656 <97842120+yogesh3656@users.noreply.github.com>
2025-07-11 14:43:39 +0530

    Create pinkTable.js

---
+++ pinkTable.js
@@ -0,0 +1,12 @@
+<script>
+window.addEventListener("load", function(){
+  document.querySelectorAll(".post-body table").forEach(function(table){
+    if (!table.parentNode.classList.contains("table-responsive")) {
+      var wrapper = document.createElement("div");
+      wrapper.className = "table-responsive";
+      table.parentNode.insertBefore(wrapper, table);
+      wrapper.appendChild(table);
+    }
+  });
+});
+</script>
