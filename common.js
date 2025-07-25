a0e44a4854a9ed6f4b5f780bec39a91ee0752409
yogesh3656 <97842120+yogesh3656@users.noreply.github.com>
2025-07-25 13:55:53 +0530

    common.js

--- common.js
+++ common.js
@@ -9,23 +9,29 @@ window.addEventListener("load", function(){
   });
 });
 
-//------------------------"table finished"/"floating btn start"----------------------------------
+//-------------------------- "table finished" / "floating btn start" ---------------------------
+
+
 // Get the button
-let mybutton = document.getElementById(&quot;myBtn&quot;);
+let mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {
+  scrollFunction();
+};
 
 function scrollFunction() {
-  if (document.body.scrollTop &gt; 20 || document.documentElement.scrollTop &gt; 20) {
-    mybutton.style.display = &quot;block&quot;;
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
   } else {
-    mybutton.style.display = &quot;none&quot;;
+    mybutton.style.display = "none";
   }
 }
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
 }
+
+
